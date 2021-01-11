56d81026c4f07004e64e74b53cf09358e9100963
shaharlab <shaharlab@shaharlab.com>
2021-01-11 11:46:18 +0000

    +model agnostic : using pip +parameter recovery

--- 01_gong_parameter recovery.R
+++ 01_gong_parameter recovery.R
@@ -9,7 +9,7 @@ source('01_functions/01_gong_functions.R')
 
 #rndwlk=readMat('myfiles/nspn_rndwlk_frcXstateXtrlXcounter.mat')[[1]]
 rndwlk=read.csv('rndwlk_v1.csv')
-Nsubj=50*2
+Nsubj=25*2
 Nexp =2
 Ntrls=1000
 
@@ -23,41 +23,119 @@ x<-do.call(rbind,x)
 source('02_models/01_gong_simme.R')
 
 df<-
-  lapply(1:Nsubj,function(s) {
+  mclapply(1:Nsubj,function(s) {
     sngdf   <-lapply(1:Nexp, function(experiment) {gong_simme(x[s,],rndwlk=rndwlk[,,],subj=s,experiment,Ntrls)})
-    sngdf   <-do.call(rbind,sngdf)})
-#    ,mc.cores =2)
+    sngdf   <-do.call(rbind,sngdf)}
+    ,mc.cores =2)
 
 
-####model agnostic
-apply(df[[2]], 2, function(x) any(is.na(x))) #are there ant NA cells 
-
+####model agnostic--------------------------------------------------------
 df2<-do.call(rbind,df)
-apply(df2, 2, function(x) any(is.na(x))) #are there ant NA cells 
+apply(df2, 2, function(x) any(is.na(x))) #are there any NA cells 
 
 library(data.table)
 library(dplyr)
 library(tidyr)
 
-df2%>%
-  mutate(rw_pv=lag(rw))
-df2$rw_pv            <-shift(df2$rw, n=1, fill=1, type=c("lag"), give.names=FALSE) # was the previous trail rewarded(=1)/unrewarded(=0) 
-df2$stay1           <-(df2$ch1==shift(df2$ch1, n=1, fill=1, type=c("lag"), give.names=FALSE)) #did the agent repeated the same first stage action 
-#df2$change1           <-(df2$ch1=!shift(df2$ch1, n=1, fill=1, type=c("lag"), give.names=FALSE)) #did the agent changed his first stage action 
-df2$stay2           <-(df2$ch2==shift(df2$ch2, n=1, fill=1, type=c("lag"), give.names=FALSE)) #did the agent repeated the same second stage action 
-df2$resp1_pv           <-shift(df2$resp1, n=1, fill=1, type=c("lag"), give.names=FALSE) #was the previous trails' first stage action type Go(=1)/Nogo(=0)
-df2$resp2_pv           <-shift(df2$resp2, n=1, fill=1, type=c("lag"), give.names=FALSE) #was the previous trails' first stage action type Go(=1)/Nogo(=0)
+df2<-df2%>%
+  mutate(rw_pv=lag(rw,default=1),
+         resp1_oneback=lag(resp1,default=1),
+         resp2_oneback=lag(resp2,default=1),
+         stay1=(ch1==lag(ch1,default=1))*1,
+         stay2=(ch2==lag(ch2,default=1))*1)
 
-df<-
-df2%>%
+
+df_2 <-
+  df2%>%
   filter(trl>1)%>%
-  group_by(subj,rw)%>%
+  group_by(subj,resp1_oneback,resp2_oneback,rw_pv)%>%
   summarise(pStay=mean(stay1))%>%
-  spread(rw,pStay)
+  pivot_wider(names_from=c(resp1_oneback,resp2_oneback,rw_pv),values_from=pStay)%>%
+  colMeans()
 
+  plot(df_2[2:9])
+
+
+#corr between true params and model-agnostic effects
+library(lme4)
+m<-glmer(stay1 ~ resp1_oneback*resp2_oneback*rw_pv + (resp1_oneback*resp2_oneback*rw_pv|subj),
+      data=df2,family=binomial(link="logit"),nAGQ = 0)
+
+library(gridExtra)
+grid.arrange(
+  plot(x[,'gamma1'],ranef(m)$subj[,'resp1_oneback:rw_pv']),
+  plot(x[,'gamma1'],ranef(m)$subj[,'resp2_oneback:rw_pv']),
+  plot(x[,'gamma2'],ranef(m)$subj[,'resp1_oneback:rw_pv']),
+  plot(x[,'gamma2'],ranef(m)$subj[,'resp2_oneback:rw_pv'])
+)
+
+
+# parameter recovery ------------------------------------------------------
+library(parallel)
+source('02_models/01_gong_fitme.R')
+fitparms<-data.frame()
+Nparms<-dim(x)[2]
+start_time <- Sys.time()
+
+#optim
+y<-
+  lapply(1:Nsubj,function(s)
+  {
+    fit<- optim(par = runif(Nparms,-3,3),
+                fn = gong_fitme,
+                df = df[[s]],
+                Ntrls = dim(df[[s]])[1],
+                lower = rep(-3,Nparms),
+                upper = rep(3,Nparms),
+                method = "L-BFGS-B")$par})
+  #mc.cores =2)
+mytime <- Sys.time() - start_time
+
+#transform and copy best fitted params
+y<-do.call(rbind,y)
+#y<-transform_params(y,c('logit','exp','logit','logit','logit','logit','logit','logit','logit','logit'))
+#y[,5:8]<-y[,5:7]-.5
+
+#model parameters
+y2<-data.frame(
+  sim.alpha1        =mylogit(y[,1]),
+#  sim.alpha2        =mylogit(y[,1]),
+  sim.gamma1        =mylogit(y[,2]),
+  sim.gamma2        =mylogit(y[,3]),
+  sim.beta1         =exp(y[,4]),
+  sim.lambda        =mylogit(y[,5]),
+  sim.persv         =mylogit(y[,6])-.5,
+  sim.go_bias       =mylogit(y[,7])-.5
+)
+
+cor(x,y2)
+diag(cor(x,y2))
+mean(diag(cor(x,y2)))
+
+
+
+
+
+
+
+
+
+####model agnostic
+apply(df[[3]], 2, function(x) any(is.na(x))) #are there ant NA cells 
+
+df2<-do.call(rbind,df)
+apply(df2, 2, function(x) any(is.na(x))) #are there ant NA cells 
+
+library(data.table)
+df2$rw_pv            <-shift(df2$rw, n=1, fill=1, type=c("lag"), give.names=FALSE) # was the previous trail rewarded(=1)/unrewarded(=0) 
+df2$stay1           <-(df2$ch1==shift(df2$ch1, n=1, fill=1, type=c("lag"), give.names=FALSE))*1 #did the agent repeated the same first stage action 
+#df2$change1           <-(df2$ch1=!shift(df2$ch1, n=1, fill=1, type=c("lag"), give.names=FALSE))*1 #did the agent changed his first stage action 
+df2$stay2           <-(df2$ch2==shift(df2$ch2, n=1, fill=1, type=c("lag"), give.names=FALSE))*1 #did the agent repeated the same second stage action 
+df2$resp1_oneback           <-shift(df2$resp1, n=1, fill=1, type=c("lag"), give.names=FALSE) #was the previous trails' first stage action type Go(=1)/Nogo(=0)
+df2$resp2_oneback           <-shift(df2$resp2, n=1, fill=1, type=c("lag"), give.names=FALSE) #was the previous trails' first stage action type Go(=1)/Nogo(=0)
 
 library(reshape2)
-df3<-dcast(df2,subj  ~ rw_pv+resp1_pv+resp2_pv,mean, value.var = c('stay1'))
+df3<-dcast(df2,subj  ~ resp1_oneback+rw_pv,mean, value.var = c('stay1'),subset=(trl>1))
 apply(df3, 2, function(x) any(is.na(x))) #are there ant NA cells 
 df3[(is.na(df3))]<- 0 #converet them to 0
 df3
@@ -65,7 +143,13 @@ df3
 df4<-colMeans(df3)
 df4
 plot(df4[2:9])
-mean(df4[2:9])
+
+
+
+
+
+
+
 
 ##################################################
 df51_p =data.frame(row.names = c("action_2_go","action_2_nogo","1st_MA"))
