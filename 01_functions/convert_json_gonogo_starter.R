ea34ccb332863c4b60386a652d96e817aaacb231
shaharlab <shaharlab@shaharlab.com>
2021-01-24 11:25:04 +0000

    convert json files to R - starter

---
+++ 01_functions/convert_json_gonogo_starter.R
@@ -0,0 +1,27 @@
+
+########################################################################
+################ convert json to Rdata  ################################
+####################################################################
+#this converts the answers to the post-task open questions from jason to data.frame
+
+#note that here prolific id is taken from the starter file
+
+con_gonogo_starter_json<-function(task_name,datafile,curnfolder,files,subnum) {
+  
+  if (sum(grepl(task_name,files))>0) {
+    df_starter<-
+      data.frame(
+        prolific_id <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[prolific-id]`,
+        num_Tries <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[numTries]`,
+        Time <- as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[endTimeStamp]`)-as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[startTimeStamp]`)
+      )
+    
+    return(df_starter)
+    #return(rbind(datafile,df_1st))
+  }
+  else{
+    return(datafile)
+  }
+}
+
+
