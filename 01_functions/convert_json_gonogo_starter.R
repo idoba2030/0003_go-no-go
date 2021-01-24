86923d1236837d883fd87193622ff8b5c4c55dc6
shaharlab <shaharlab@shaharlab.com>
2021-01-24 13:15:25 +0000

    fixing the situation of multiple starter files

--- 01_functions/convert_json_gonogo_starter.R
+++ 01_functions/convert_json_gonogo_starter.R
@@ -8,16 +8,25 @@
 
 con_gonogo_starter_json<-function(task_name,datafile,curnfolder,files,subnum) {
   
-  if (sum(grepl(task_name,files))>0) {
+  if (sum(grepl(task_name,files))==1) {
     df_starter<-
       data.frame(
-        prolific_id <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[prolific-id]`,
-        num_Tries <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[numTries]`,
-        Time <- as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[endTimeStamp]`)-as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)],sep=""))$`data[startTimeStamp]`)
+        prolific_id <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[prolific-id]`,
+        num_Tries <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[numTries]`,
+        Time <- as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[endTimeStamp]`)-as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[startTimeStamp]`)
       )
     
-    return(df_starter)
-    #return(rbind(datafile,df_1st))
+    return(rbind(datafile,df_starter))
+  }
+  else if (sum(grepl(task_name,files))>1){
+    df_starter<-
+      data.frame(
+        prolific_id <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[prolific-id]`,
+        num_Tries <- fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[numTries]`,
+        Time <- as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[endTimeStamp]`)-as.integer(fromJSON(file=paste(curnfolder,'/',files[grepl('starter',files)][1],sep=""))$`data[startTimeStamp]`)
+      )
+    
+    return(rbind(datafile,df_starter))
   }
   else{
     return(datafile)
