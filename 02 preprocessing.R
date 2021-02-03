b9f4886b6d3b70260b01c693138e15d39985a08a
shaharlab <shaharlab@shaharlab.com>
2021-02-03 07:34:51 +0000

    #

---
+++ 02 preprocessing.R
@@ -0,0 +1,4 @@
+load(cards,file='myfolder/03_data/02_aggregated_data/cards_raw.Rdata')
+load(questions,file='myfolder/03_data/02_aggregated_data/questions_raw.Rdata')
+save(screensize,file='myfolder/03_data/02_aggregated_data/screensize_raw.Rdata')
+save(squares,file='myfolder/03_data/02_aggregated_data/squares_raw.Rdata')
